521c23303d7cf379eb9590c9373f7228138cc0b7
Nabeel Sulieman <nabsul@users.noreply.github.com>
2021-09-20 10:55:00 -0700

    Some prettifying

--- nodejs/generate-data.js
+++ nodejs/generate-data.js
@@ -1,48 +1,26 @@
-const fname = 'data.json'
-const numTags = 100
-const startDate = new Date('2021-09-01')
-const endDate = new Date('2021-10-01')
-
 const { open } = require('fs/promises')
 
-const generateEntry = (date) => {
-    const userId = Math.floor(Math.random() * numTags)
-    const val = Math.floor(Math.random() * 100)
-
-    return {
-        time: date.toISOString(),
-        userId: `user${userId}`,
-        points: val
-    }
-}
-
-const incrementDate = (date) => new Date(date.getTime() + Math.floor(Math.random() * 1000 * 10))
-
 const writeData = async () => {
+    const startDate = new Date('2021-09-01')
+    const endDate = new Date('2021-10-01')
+
     console.log('opening file output...')
-    var file = await open(fname, 'w')
+    const file = await open('data.json', 'w')
 
+    let prefix = '[\n  '
+    let date = new Date(startDate.getTime() + Math.floor(Math.random() * 1000 * 10))
     console.log('generating data (this could take a while)...')
-    await file.write('[')
-
-    let date = incrementDate(startDate)
-    let first = true
     while (date < endDate) {
-        if (first) {
-            first = false
-            await file.write('\n\t')
-        } else {
-            await file.write(',\n\t')
-        }
-
-        await file.write(JSON.stringify(generateEntry(date)))
-        date = incrementDate(date)
+        const userId = Math.floor(Math.random() * 100)
+        const points = Math.floor(Math.random() * 100)
+        const data = { time: date.toISOString(), userId: `user${userId}`, points: points }
+        await file.write(prefix + JSON.stringify(data))
+        prefix = ',\n  '
+        date = new Date(date.getTime() + Math.floor(Math.random() * 1000 * 10))
     }
 
-    await file.write('\n]')
-
+    await file.write('\n]\n')
     console.log('done.')
-    await file.close()
 }
 
 writeData()
